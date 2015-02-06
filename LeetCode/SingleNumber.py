0e66de643682fd38233042b3b3fd33b1d1b05dd3
geeeeeeeeek <810421910@qq.com>
2015-02-06 23:02:31 +0800

    94ms

--- LeetCode/SingleNumber.py
+++ LeetCode/SingleNumber.py
@@ -5,12 +5,12 @@ class Solution:
     # @param A, a list of integer
     # @return an integer
     def singleNumber(self, A):
-        n = len(A) - 1
-        while 0 < n:
+        for n in range(1, len(A))[::-1]:
             A[n - 1] ^= A[n]
-            n -= 1
         return A[0]
 
 
 solution = Solution().singleNumber([1, 1, 2, 4, 2, 3, 4])
+print(solution)
+solution = Solution().singleNumber([1])
 print(solution)
